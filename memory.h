3102da807e319e70e80e34e4823667084fc3c85b
jrmccluskey <thejackmccluskey@gmail.com>
2020-06-09 19:12:26 -0400

    Added note for memory accesses

--- memory.h
+++ memory.h
@@ -12,10 +12,13 @@ public:
         mem[31] = 42;
     }
 
+    // Remember that you're pulling memory addresses but the
+    // representation in code is in arrays so you have to convert
     inline int calcIndex(uint8_t lowBits, uint8_t hiBits) {
         uint16_t addr = ((hiBits << 8) | lowBits);
         int index = addr/8;
         if(index > 8191) {
+            // Out-of-bounds memory accesses wrap around
             index = index % 8192;
         }
         return index;
